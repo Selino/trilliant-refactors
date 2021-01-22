c837bb4ef6d2e37e6fbe0824a895d5112d8015d4
Selino Valdes <selinov@gmail.com>
2021-01-22 00:43:43 -0800

    Updates

--- src/components/MiniBar.js
+++ src/components/MiniBar.js
@@ -2,6 +2,15 @@ import React from "react"
 
 /**
 An inline bar chart that accepts the Perc property to calculate the visualized metric.
+
+### UX Notes
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla massa, viverra quis est non, mollis aliquam turpis. Aliquam vitae gravida justo. Sed nec venenatis risus, non sollicitudin erat. Aenean bibendum dolor sit amet mi tristique, quis tincidunt purus tincidunt. Vestibulum auctor dui id lorem pulvinar auctor. Praesent nec egestas purus. Vestibulum porta tempor ornare.
+
+- [Wireframe](https://duckduckgo.com)
+- [Prototype](https://duckduckgo.com)
+- [User Journey Map](https://duckduckgo.com)
+- [A/B Testing Results](https://duckduckgo.com)
+- [Full Report](https://duckduckgo.com)
 **/
 
 export default function MiniBar(props) {
